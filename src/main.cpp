9e14c8ca20fe16475be2f12b1c41a2ead35c491b
Arthur Aznive <61330561+Amoskeag@users.noreply.github.com>
2022-08-12 15:53:05 -0400

    Fix
    Updated

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+#include "MM5451.h"
+
+void setup() {
+  // put your setup code here, to run once:
+  // Create a number of MM5451 instances for the number you will be using.
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  // Write your driver code here.
+}
