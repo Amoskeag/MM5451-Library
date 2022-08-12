cdf0d26f89989ed222c85a3108cfe66f10d6d16c
KenwoodFox <kenwood364@gmail.com>
2022-08-12 15:19:22 -0400

    Remove dev junk, move pio to examples folder, ignore binaries, create library.properties, update metadata, template source code.

---
+++ src/MM5451.cpp
@@ -0,0 +1,9 @@
+/**
+ * @file MM5451.cpp
+ * @author your name (you@domain.com)
+ * @brief Methods and functions for the MM5451 library.
+ * @date 2022-08
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
---
+++ src/MM5451.h
@@ -0,0 +1,9 @@
+/**
+ * @file MM5451.h
+ * @author your name (you@domain.com)
+ * @brief Prototypes for the MM5451 library.
+ * @date 2022-08
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
