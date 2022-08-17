20b8707fda14cb7d3ded791e1f6431d51beaeb5b
Arthur Aznive <arthur.aznive@gmail.com>
2022-08-17 13:05:57 -0400

    Add Matrix Manipulation
    Add Lookup table for 7-segment displays. Add Shift Left and Right

--- src/MM5451.cpp
+++ src/MM5451.cpp
@@ -17,10 +17,74 @@ MM5451::LED_Driver(int DataPin, int CLK)
 	_CLK = CLK;
 }
 
+MM5451::getDataPin()
+{
+	//returns the assigned DataPin value.
+	return _DataPin;
+}
+
 MM5451::pulseCLK()
 {
 	digitalWrite(_CLK, 1);
 	digitalWrite(_CLK, 0);
+}
+
+MM5451::ShiftDataLeft(int (&data)[35])
+{
+	//Shift Data to the Left. First element becomes last "34th" element.
+
+	int temp = data[0]; //Hold First
+
+    for (int i = 0; i < n - 1; i++)
+    {
+      data[i] = data[i + 1]; //move all element to the left except first one
+    }
+
+    data[n - 1] = temp;
+
+  	return data;
+}
+
+MM5451::ShiftDataRight(int (&data)[35])
+{
+	//Shift Data to the Right, Last element becomes the first "zero-th"  element.
+
+	int temp = data[0]; //Hold First 
+	
+    for (int i = 0; i < 34; i++)
+    {
+      data[i + 1] = data[i]; //move all element to the Right except first one
+    }
+
+    data[34] = temp; //Set the held value to new position.
+
+  	return data;
+}
+
+//address the pins of the selected 7 seg display.
+MM5451::displaySSeg(Bool b, int (&data)[35])
+{
+
+	//Update Data and send it. 
+	if( b )
+	{
+		//CC, Logic 1 on. Read table directly
+
+		
+	}
+	if(!b)
+	{
+		//CA, Logic 0 on. Invert Table
+
+	}
+	else
+	{
+		//Error
+	}
+
+	//SendData(data);
+
+
 }
 
 //SendData is given a 35 Bits
--- src/MM5451.h
+++ src/MM5451.h
@@ -20,17 +20,60 @@ public:
 	//Constructor
 	LED_Driver(int DataPin, int CLK);
 
-	//Methods
+	/*
+	* 	METHODS
+	*/
 
-	//returns the pin this Driver is using.
+	//returns the pin value this Driver is using. Returns int.
 	getDataPin();
 
-	//Scroll the data held in the 
-	ScrollData();
+	//Scroll the data held in the Array Left. Returns array.
+	ShiftDataLeft(int (&data)[35]);
+
+	//Scroll the data held in the Array Right. Returns array.
+	ShiftDataRight(int (&data)[35]);
+
+	/*Displays Characters on a 7 segment display. Boolean defines
+	* if you are using Common Cathode or Common Annode (True CC, False CA).
+	*/
+	displaySSeg(bool b, int (&data)[35]);
+
+
 
 private:
 	int _DataPin;
 	int _CLK;
+
+	/**
+	 * @brief Look up table for 7-Seg Displays
+	 * 
+	 * 	Each MM5451 can support 4 - 5, 7 Seg Displays
+	 *	Display 1 = Bits 1 - 7
+	 *	Display 2 = Bits 8 - 14
+	 * 	Display 3 = Bits 15 - 21
+	 *	Display 4 = Bits 22 - 28
+	 *	Display 5 = Bits 29 - 35
+	 */
+	
+	uint8_t lookUpTable[] = {
+		0x1111110,	//0
+		0x110000,	//1
+		0x1101101,	//2
+		0x1111001,	//3
+		0x0110011,	//4
+		0x1011011,	//5
+		0x1011111,	//6
+		0x1110000,	//7
+		0x1111111,	//8
+		0x1111011,	//9
+		0x1110111,	//A
+		0x0011111,	//B
+		0x1001110,	//C
+		0x0111101,	//D
+		0x1001111,	//E
+		0x1000111	//F
+
+	}
 
 	//Sends 35 bits of data to the board to output
 	/**
